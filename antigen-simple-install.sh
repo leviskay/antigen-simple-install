93898bab136f14d54c2790f17956d4d39b662e2c
leviskay <leviathan.po4ta@gmail.com>
2021-01-03 01:33:53 +0300

    add installZshForUser func

--- antigen-simple-install.sh
+++ antigen-simple-install.sh
@@ -1,13 +1,43 @@
 #!/bin/bash
 
+pathToAntigen="/usr/share/antigen"
+scriptName="antigen.zsh"
+fullPath=$pathToAntigen/$scriptName
+
+function installZshForUser {
+  user=$1
+  userDir=$2
+  while true; do
+    read -p "Install zsh as default shell and write .zshrc for $user?[y/N] " yn
+    case $yn in
+      [Yy]* ) break;;
+      [Nn]* ) return;;
+      * ) return;;
+    esac
+  done
+  zshrc="source $fullPath 
+  antigen use oh-my-zsh
+  antigen bundle git
+  antigen bundle heroku
+  antigen bundle pip
+  antigen bundle lein
+  antigen bundle command-not-found
+  antigen bundle zsh-users/zsh-syntax-highlighting
+  antigen theme terminalparty
+  antigen apply"
+
+  echo -n "Write .zshrc for $user..."
+  echo "$zshrc" > $userDir/.zshrc
+  echo " Success"
+  echo -n "Install zsh as default shell for $user..."
+  chsh -s /bin/zsh $user 
+  echo " Success"
+}
+
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-
-pathToAntigen="/usr/share/antigen"
-scriptName="antigen.zsh"
-fullPath=$pathToAntigen/$scriptName
 
 if [ -f $fullPath ]; then 
   echo "Antigen has already been installed"
@@ -19,20 +49,5 @@ apt install -y curl zsh
 mkdir -p $pathToAntigen 
 
 curl -L git.io/antigen > $pathToAntigen/antigen.zsh
-zshrc="source $fullPath 
-antigen use oh-my-zsh
-antigen bundle git
-antigen bundle heroku
-antigen bundle pip
-antigen bundle lein
-antigen bundle command-not-found
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen theme terminalparty
-antigen apply"
 
-echo "Write .zshrc for root..."
-echo "$zshrc" > /root/.zshrc
-echo "Success"
-echo "Install zsh as default shell for root..."
-chsh -s /bin/zsh root
-echo "Success"
+installZshForUser root /root
